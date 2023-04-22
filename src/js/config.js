6c1fa4f033b63a1327833a67d83f1e268737ef26
Yusufbek01 <Yusufbek1771@gmail.com>
2023-04-22 17:55:04 +0500

    incial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = "https://forkify-api.herokuapp.com/api/v2/recipes/";
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 11;
+export const KEY = '7837d6cd-8c0d-4b37-9ee1-fa2d3cdb88e3';
+export const MODEL_CLOSE_SEC = 2;
---
+++ src/js/views/resultsView.js
@@ -0,0 +1,19 @@
+import View from "./view";
+import previewView from "./previewView";
+import icons from 'url:../../img/icons.svg';
+
+
+class resultView extends View {
+  _parentElement= document.querySelector(".results");
+
+  _errorMessage = `No recipe found for your query! Please try again;`;
+  _message = ``;
+
+  _generateMarkup() {
+    return this._data
+    .map(result => previewView.render(result, false))
+    .join("");
+  };
+    
+};
+export default new resultView();
